5ff4021ac15e97c2e277737f239b2bc177524c7a
omjeejaiswal <sureshjaiswal808@gmail.com>
2024-08-15 00:14:11 +0530

    .

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,14 +1,18 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const http_1 = __importDefault(require("http"));
 const express = require('express');
-const { createServer } = require('node:http');
 const { join } = require('node:path');
 const { Server } = require('socket.io');
 const app = express();
-const io = new Server(app);
+const server = http_1.default.createServer(http_1.default);
+const io = new Server(server);
 io.on('connection', (socket) => {
     console.log('a user connented');
 });
-app.listen(3000, () => {
+server.listen(3000, () => {
     console.log('server running at http://localhost:3000');
 });
