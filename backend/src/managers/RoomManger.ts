c8458f1df43b07a000a301bd4c24a732d44c695e
omjeejaiswal <sureshjaiswal808@gmail.com>
2024-08-16 23:35:02 +0530

    correction onOffer

--- backend/src/managers/RoomManger.ts
+++ backend/src/managers/RoomManger.ts
@@ -27,15 +27,15 @@ export class RoomManager {
     }
 
     onOffer(roomId: string, sdp : string) {
-        const user2 = this.rooms.get(roomId)?.user1;
+        const user2 = this.rooms.get(roomId)?.user2;
         user2?.socket.emit("offer", {
             sdp
         })
     }
 
     onAnswer(roomId: string, sdp : string) {
-        const user2 = this.rooms.get(roomId)?.user1;
-        user2?.socket.emit("offer", {
+        const user1 = this.rooms.get(roomId)?.user1;
+        user1?.socket.emit("offer", {
             sdp
         })
     }
