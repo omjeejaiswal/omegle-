075e10142a7d045b55d38807d039eaa12918c40b
omjeejaiswal <sureshjaiswal808@gmail.com>
2024-08-16 11:54:02 +0530

    connecting web socket

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,6 @@
 import { Socket } from "socket.io";
 import http from "http"
 const express = require('express')
-const {join} = require('node:path');
 const {Server} = require('socket.io');
 
 const app = express();
