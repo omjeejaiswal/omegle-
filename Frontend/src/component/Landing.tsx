4959aa1910aa2454460cd1c662378dbaf734f6a4
omjeejaiswal <sureshjaiswal808@gmail.com>
2024-08-25 12:24:39 +0530

    .

--- Frontend/src/component/Landing.tsx
+++ Frontend/src/component/Landing.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useRef, useState } from "react"
-import { Link } from "react-router-dom";
 import { Room } from "./Room";
 
 export const Landing = () => {
@@ -56,3 +55,42 @@ export const Landing = () => {
     
 }
 
+
+
+
+
+// import { useEffect, useState } from "react";
+// import { Link } from "react-router-dom";
+// import Send from "../assets/sendBg.png";
+
+// export const Landing = () => {
+//   const [name, setName] = useState("");
+//   const [joined, setJoined] = useState(false);
+
+//   useEffect(() => {}, []);
+
+//   return (
+//     <div className="main">
+//       <div>
+//         <h1 className="s-1">Omegle 2.0</h1>
+//         <h1 className="s-2">Connect, Chat, Discover</h1>
+//         <div className="submit">
+//           <input
+//             type="text"
+//             placeholder="Enter your name"
+//             className="input"
+//             onChange={(e) => {
+//               setName(e.target.value);
+//             }}
+//           ></input>
+//           <Link to={`/room/?name=${name}`}>
+//             <button>
+//               {" "}
+//               <img src={Send} />
+//             </button>
+//           </Link>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
